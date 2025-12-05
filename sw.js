8bd8596674820912b057e139ffdbbb9b751f0c0d
Dave Burt <dave@burt.id.au>
2025-12-05 12:49:06 +1100

    Add PWA support with manifest and service worker

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'special-chess-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icon.svg',
+  '/manifest.json',
+  'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
